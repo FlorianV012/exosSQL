f900d2c2772e28de4b8de9e6d8eba9ff3f1bb58d
vfloria <fvilmot@gmail.com>
2022-12-06 15:36:47 +0100

    page 7

--- SQL1.1-07-Requêtes-SELECT-complexes.sql
+++ SQL1.1-07-Requêtes-SELECT-complexes.sql
@@ -3,9 +3,21 @@
 
 /* RESULTAT ==> 38 lignes / 146 / 130305.35 */
 
-
+SELECT payments.customerNumber, contactLastName, SUM(payments.amount) AS total, country FROM `payments`
+JOIN customers
+ON payments.customerNumber = customers.customerNumber
+WHERE country IN ("France", "USA", "Germany")
+GROUP BY payments.customerNumber
+HAVING SUM(payments.amount)> 50000
+ORDER BY country, SUM(payments.amount) DESC;
 
 /* Le montant total de chaque commande (numéro et date) des clients New-Yorkais (nom) trié par nom du client puis par date de commande */
 
+SELECT ROUND(SUM(orderdetails.quantityOrdered*orderdetails.priceEach),2) as amountTotal, orders.orderNumber, orders.orderDate, customerName FROM customers
+JOIN orders ON customers.customerNumber = orders.customerNumber
+JOIN orderdetails ON orders.orderNumber = orderdetails.orderNumber
+WHERE city LIKE "NYC"
+GROUP BY orders.orderNumber
+ORDER BY customers.customerName, orders.orderDate;
 
 /* RESULTAT ==> 16 lignes / Classic Legends / 10115 / 21665.98 */
