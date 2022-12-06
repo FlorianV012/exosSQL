6c8e7c64765227e63b09f9ad1bc647478773f237
vfloria <fvilmot@gmail.com>
2022-12-06 13:01:10 +0100

    page 6

--- SQL1.1-06-Clause-INNER-JOIN-Jointures.sql
+++ SQL1.1-06-Clause-INNER-JOIN-Jointures.sql
@@ -1,5 +1,8 @@
 /* La liste des employés (nom, prénom et fonction) et des bureaux (adresse et ville) dans lequel ils travaillent */
 
+SELECT firstName, lastName, addressLine1, city FROM `employees`
+JOIN offices
+ON employees.officeCode = offices.officeCode;
 
 /* RESULTAT ==> 23 lignes / Diane Murphy */
 
@@ -7,6 +10,11 @@
 
 /* La liste des clients français ou américains (nom du client, nom, prénom du contact et pays) et de leur commercial dédié (nom et prénom) triés par nom et prénom du contact */
 
+SELECT customerName, contactFirstName, contactLastName, country, lastName , firstName FROM `customers`
+JOIN employees
+ON customers.salesRepEmployeeNumber =  employees.employeeNumber
+WHERE country IN("France", "USA")
+ORDER BY contactLastName, contactFirstName;
 
 /* RESULTAT ==> 48 lignes / Miguel Barajas */
 
@@ -14,5 +22,6 @@
 
 /* La liste des lignes de commande (numéro de commande, code, nom et ligne de produit) et la remise appliquée aux voitures ou motos commandées triées par numéro de commande puis par remise décroissante */
 
+SELECT orderNumber, orderdetails.productCode, products.productName, productline, MSRP - priceEach AS discount FROM `orderdetails` JOIN products ON products.productCode = orderdetails.productCode WHERE productline LIKE "Motorcycles" OR productline LIKE "%Car%" ORDER BY orderNumber, `discount` DESC;
 
 /* RESULTAT ==> 2026 lignes / 34 */
