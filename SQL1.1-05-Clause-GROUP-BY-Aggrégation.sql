8be2412dc021a0966291841f0998aaa0923d4d2c
vfloria <fvilmot@gmail.com>
2022-12-06 12:11:18 +0100

    page 5

--- SQL1.1-05-Clause-GROUP-BY-Aggrégation.sql
+++ SQL1.1-05-Clause-GROUP-BY-Aggrégation.sql
@@ -1,5 +1,8 @@
 /* Le prix moyen d'un produit vendu par chaque vendeur triés par prix moyen décroissant */
 
+SELECT productVendor, AVG(MSRP) AS averagePrice FROM `products`
+GROUP BY productVendor
+ORDER BY averagePrice DESC;
 
 /* RESULTAT ==> 13 lignes / Welly Diecast Productions / 113.9325 */
 
@@ -7,6 +10,8 @@
 
 /* Le nombre de produits pour chaque ligne de produit */
 
+SELECT productLine, COUNT(productName) FROM `products`
+GROUP  BY productLine;
 
 /* RESULTAT ==> 7 lignes / Classic Cars / 38 */
 
@@ -17,10 +22,16 @@
 
 /* RESULTAT ==> 7 lignes / Ships / 429177.74 */
 
-
+SELECT productLine, SUM(quantityInStock) AS QuantityTotal, SUM(quantityInStock * MSRP) AS stockValue FROM `products`
+WHERE MSRP > 100
+GROUP  BY productLine
+ORDER BY stockValue;
 
 /* La quantité du produit le plus en stock de chaque vendeur trié par vendeur */
 
+SELECT productVendor, MAX(quantityInStock) FROM `products`
+GROUP  BY productVendor
+ORDER BY productVendor;
 
 /* RESULTAT ==> 13 lignes / Autoart Studio Design / 9354 */
 
@@ -28,6 +39,8 @@
 
 /* Le prix de l'avion qui coûte le moins cher à l'achat */
 
+SELECT MIN(buyPrice) FROM `products`
+WHERE productLine="Planes";
 
 /* RESULTAT ==> 1 ligne / 29.34$ */
 
@@ -35,5 +48,9 @@
 
 /* Le crédit des clients qui ont payé plus de 20000$ durant l'année 2004 trié par crédit décroissant */
 
+SELECT customerNumber, ROUND(SUM(amount),2) AS totalAmount FROM `payments`
+WHERE paymentDate LIKE "2004%"
+GROUP BY customerNumber HAVING SUM(amount) > 20000
+ORDER BY totalAmount DESC;
 
 /* RESULTAT ==> 69 lignes / 141 / 293 765.51 */
